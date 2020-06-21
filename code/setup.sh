deff8b9b225a26a0a3346ff83b2b2fcf9f54830b
Greig Sheridan <greiginsydney@users.noreply.github.com>
2020-06-21 19:41:13 +1000

    Added node to the $PATH env variable

--- code/setup.sh
+++ code/setup.sh
@@ -41,6 +41,13 @@ step1 ()
 		wget https://nodejs.org/dist/latest-v11.x/node-v11.15.0-linux-armv6l.tar.gz
 		tar -xzf node-v11.15.0-linux-armv6l.tar.gz
 		mv node-v11.15.0-linux-armv6l /usr/local/
+		if [[ ":$PATH:" == *":/usr/local/node-v11.15.0-linux-armv6l/bin:"* ]]; then
+			echo "Already present in the PATH"
+		else
+			sh -c "echo export PATH=$PATH:/usr/local/node-v11.15.0-linux-armv6l/bin >> /etc/profile"
+			source /etc/profile
+			echo "Added to the PATH"
+		fi
 		apt-get update
 	else
 		curl -sl https://deb.nodesource.com/setup_12.x | sudo -E bash -
